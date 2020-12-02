fcd3cb0f3a7c0e400d43e15a8a64d41fcd3ea506
Raul Neto <raulneto90@gmail.com>
2020-12-02 11:24:07 -0300

    Route applications

--- src/index.js
+++ src/index.js
@@ -2,8 +2,36 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
+app.use(express.json());
+
+app.get('/projects', (request, response) => {
+  const { title, owner } = request.query;
+
+  console.log(title, owner);
+
+  return response.json(['Projeto 1', 'Projeto 2']);
+});
+
+app.post('/projects', (request, response) => {
+  const { title, owner } = request.body;
+
+  return response.json(['Projeto 1', 'Projeto 2', 'Projeto 3']);
+});
+
+app.put('/projects/:id', (request, response) => {
+  const { id } = request.params;
+
+  console.log(id);
+
+  return response.json(['Projeto teste 1', 'Projeto 2', 'Projeto 3']);
+});
+
+app.delete('/projects/:id', (request, response) => {
+  const { id } = request.params;
+
+  console.log(id);
+
+  return response.json(['Projeto teste 1', 'Projeto 3']);
 });
 
 app.listen(3333, () => {
